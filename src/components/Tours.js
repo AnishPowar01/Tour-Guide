eefffbc12626687caa32959c07fda53a40ff67bb
AnishPowar01 <powaranish74@gmail.com>
2023-03-16 23:03:47 +0530

    initial-commit

---
+++ src/components/Tours.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from './Card'
+
+function Tours({tours,removeTour}) {
+  return (
+    <div className='container'>
+    <div><h2 className='title'>Plan With Anu</h2></div>
+
+     <div className='cards'>
+     {
+        tours.map((tour) =>{
+            return (
+                <Card {...tour} removeTour = {removeTour} />
+            )
+        })
+     }
+        
+     </div>
+
+    </div>
+  )
+}
+
+export default Tours
